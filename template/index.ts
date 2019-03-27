fac44e111d389a6e9fd7f11f2db8ae36ce203407
孙玮祺 <92193905@qq.com>
2019-03-27 09:51:30 +0800

    更新css模板

--- template/index.ts
+++ template/index.ts
@@ -1,6 +1,3 @@
-// import GoodsApi from 'scrat-apis-person-interest/GoodsApi'
-
-
 const INIT_STATE = {
 
 }
@@ -25,45 +22,8 @@ export default {
     }
   },
   effects: {
-    // // DESC: 或者使用put.resolve
-    // * getData ({ payload: { id } }, { put }) {
-    //   yield put({
-    //     type: "getBaseData",
-    //     payload: {
-    //       id
-    //     }
-    //   })
+    * getData ({ payload: { id } }, { put }) {
 
-    //   yield put({
-    //     type: "getExtraData",
-    //     payload: {
-    //       id
-    //     }
-    //   })
-    // },
-    // // 基本信息
-    // * getBaseData ({ payload: { id } }, { put, call }) {
-    //   const res = yield call(GoodsApi.getById, id)
-    //   yield put({
-    //     type: 'save',
-    //     payload: {
-    //       goodsInfo: res,
-    //     }
-    //   })
-    // },
-    // // 给予基本信息的额外信息
-    // * getExtraData ({ payload: { id } }, { take, select, call, put }) {
-    //   yield take("getProduct/@@end")
-    //   const goodsInfo = yield select(s => s._MODULE_NAME_)
-    //   if (goodsInfo.type === 'REAL') {
-    //     const res = yield call(GoodsApi.getSkuGroupList, id)
-    //     yield put({
-    //       type: 'save',
-    //       payload: {
-    //         skuInfo: res,
-    //       }
-    //     })
-    //   }
-    // },
+    },
   },
 }
--- template/index.tsx
+++ template/index.tsx
@@ -1,11 +1,14 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View } from '@tarojs/components'
 import { connect } from '@tarojs/redux';
+import get from 'lodash/get'
 
 import Loading from '@/components/loading'
 
+import classNames from 'classnames/bind'
 import style from './index.module.less'
 
+const cx = classNames.bind(style)
 
 interface IProps {
   dispatch: ({ type: string, payload: any }) => Promise<any>
@@ -26,12 +29,7 @@ class Index extends Component<IProps> {
 
 
   componentDidMount = () => {
-    // this.props.dispatch({
-    //   type: "_MODULE_NAME_/getDataById",
-    //   payload: {
-    //     id: this.$router.params.id
-    //   }
-    // })
+
   }
 
   componentWillUnmount = () => {
@@ -46,9 +44,9 @@ class Index extends Component<IProps> {
   render () {
     const isLoading = this.props.loading.models._MODULE_NAME_
     return (
-      <View className={style['page--wrapper']}>
+      <View className={cx('page')}>
         {isLoading && <Loading />}
-        <View className={style['page']}>
+        <View className={cx('container')}>
 
         </View>
       </View>
