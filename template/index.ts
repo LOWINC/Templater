62b08c192f8718556a1df1c2816df18de32b7a73
sunweiqi <921939025@qq.com>
2019-06-17 16:13:42 +0800

    依赖的状态

--- template/index.ts
+++ template/index.ts
@@ -22,13 +22,53 @@ export default {
     },
     // DESC: immerPlugin
     // https://github.com/dvajs/dva/tree/master/packages/dva-immer
-    // updateStatus (state, { payload: { status } }) {
-    //   state.status = status
-    // }
+    immer (state, { payload: { handler } }) {
+      handler(state)
+    },
   },
   effects: {
     * getData ({ payload: { id } }, { put }) {
 
     },
+
+    * getPageData ({ }, { select, take }) {
+      const {
+        appConfig: {
+          isAjaxed,
+          data: {
+            isShowNav,
+            isShowIndex
+          }
+        }
+      } = yield select(s => s.global)
+
+      if (!isAjaxed) {
+        yield take('global/getAppConfig/@@end')
+      }
+
+      if (isShowNav) {
+        // doSth
+      }
+
+
+      if (isShowIndex) {
+        // doSth
+      }
+
+    },
+
+    * updateByImmer ({ }, { put }) {
+      const user = {
+        name: "swq",
+        job: "web"
+      }
+
+      yield put({
+        type: "immer",
+        payload: store => {
+          store.user = user
+        }
+      })
+    }
   },
 }
